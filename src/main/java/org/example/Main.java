35fd6c766bbbd40813bf6a2497c18088e9cf1c86
Arif Ikhsanudin <arif.ikhsanudin@mitrais.com>
2022-06-27 12:38:15 +0700

    add read and write example

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,28 @@
 package org.example;
 
+import java.io.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        var fileName = "src/main/java/org/example/output.txt";
+        try {
+            var bufferedWriter = new BufferedWriter(new FileWriter(fileName));
+            bufferedWriter.write("Arif\n");
+            bufferedWriter.write("Ikhsanudin\n");
+            bufferedWriter.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        try {
+            var bufferedReader = new BufferedReader(new FileReader(fileName));
+            var line = bufferedReader.readLine();
+            while (line != null) {
+                System.out.println(line + ' ' + line.length());
+                line = bufferedReader.readLine();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
